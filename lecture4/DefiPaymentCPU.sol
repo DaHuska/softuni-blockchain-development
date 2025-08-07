c6c453898b7f200707847b20929b27389f3cfb86
DaHuska <aboyanov@abv.bg>
2025-08-07 22:53:41 +0300

    Add refunding

--- lecture4/DefiPaymentCPU.sol
+++ lecture4/DefiPaymentCPU.sol
@@ -2,10 +2,11 @@
 pragma solidity 0.8.28;
 
 contract PaymentProcessor {
-    mapping(address => uint256) balances;
-    mapping(address => uint256[]) payments;
+    mapping(address => uint256) public balances;
+    mapping(address => uint256[]) public payments;
 
     function receivePayment(uint256 _payment) payable external {
+        balances[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] += 10;
         require(balances[msg.sender] >= _payment);
 
         balances[msg.sender] -= _payment;
@@ -16,16 +17,16 @@ contract PaymentProcessor {
         return balances[msg.sender];
     }
 
-    function refundPayment() internal virtual {
+    function refundPayment(uint256 _transaction) payable public virtual {
 
     }
 }
 
 contract Merchant is PaymentProcessor {
-    function refundPayment() internal override {
-        require(payments[msg.sender][0] > 0);
-
+    function refundPayment(uint256 _transaction) payable public override {
         // remove payment from array
-        balances[msg.sender] += payments[msg.sender][0];
+        balances[msg.sender] += payments[msg.sender][_transaction];
+        payments[msg.sender][_transaction] = payments[msg.sender][payments[msg.sender].length - 1];
+        payments[msg.sender].pop();
     }
 }
